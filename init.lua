a3d677c20a20082e7c5312baae0652a5aebcb1e9
paramat <mat.gregory@virginmedia.com>
2013-05-17 23:46:26 +0100

    Version 0.2.0, ongen / abm, jungletree debris, bug fixes

--- init.lua
+++ init.lua
@@ -1,35 +1,77 @@
--- meseor 0.1.0 by paramat.
+-- meseor 0.2.0 by paramat.
 -- License WTFPL, see license.txt.
 
 -- Parameters.
 
-local IMPACT = true -- (true / false) -- Enable / disable impacts.
+local ONGEN = true -- (true / false) Enable / disable impacts on generated chunk.
+local ONGCHA = 9 -- 9 -- Ongen 1/x chance of impact per generated chunk.
+
+local ABM = false -- Enable / disable impacts by abm on surface material.
+local ABMINT = 181 -- 181 -- Abm interval.
+local ABMCHA = 100000 -- 100000 -- Abm 1/x chance per node.
 local DAMAGE = true -- Enable / disable player damage loop.
 local DAMMAX = 20 -- 20 -- Maximum damage. 20 = direct hit is fatal.
-local MSRINT = 181 --  -- Meseor abm interval.
-local MSRCHA = 100000 --  -- Meseor 1/x chance per node.
-local RADMIN = 6 -- 6 -- Minimum crater radius.
-local RADMAX = 32 -- 32 -- Maximum crater radius.
+
+local RADMIN = 3 -- 3 -- Minimum crater radius.
+local RADMAX = 31 -- 31 -- Maximum crater radius, on-gen craters are limited to 16.
 local STOCHA = 5 -- 5 -- 1/x chance of stone boulders instead of gravel.
+
+local XMIN = -16000 -- Impact area dimensions. Impacts only inside this area.
+local XMAX = 16000
+local ZMIN = -16000
+local ZMAX = 16000
+
+local SAXMIN = 0 -- Safe area dimensions. No impacts inside this area.
+local SAXMAX = 0 -- When overlapping impact area, the safe area overrides.
+local SAZMIN = 0
+local SAZMAX = 0
+
 local DEBUG = true
-
-local XMIN = -16384 -- Impact area dimensions. Impacts only inside this area.
-local XMAX = 16384
-local ZMIN = -16384
-local ZMAX = 16384
-
-local SAXMIN = 30000 -- Safe area dimensions. No impacts inside this area.
-local SAXMAX = 30000 -- When overlapping impact area, the safe area overrides.
-local SAZMIN = 30000
-local SAZMAX = 30000
 
 -- Stuff.
 
 meseor = {}
 
--- Meseor abm.
+-- On generated.
 
-if IMPACT then
+if ONGEN then
+	minetest.register_on_generated(function(minp, maxp, seed)
+		if math.random(ONGCHA) == 2 then
+			local env = minetest.env
+			local maxrad = RADMAX
+			if maxrad > 16 then maxrad = 16 end
+			local conrad = math.random(RADMIN, maxrad)
+			local rimrad = math.ceil(conrad * 2.4)
+			local x = math.random(minp.x + rimrad, maxp.x - rimrad)
+			local z = math.random(minp.z + rimrad, maxp.z - rimrad)
+			-- Find surface with air above, abort if water found, ignore trees and ice.
+			local surfy = false
+			local aa = false
+			for y = maxp.y, minp.y, -1 do
+				local nodename = env:get_node({x=x,y=y,z=z}).name
+				if nodename == "default:water_source" or nodename == "default:water_flowing" then
+					return
+				elseif nodename == "air" then
+					aa = true
+				elseif aa and nodename ~= "air" and nodename ~= "ignore" and nodename ~= "snow:ice"
+				and nodename ~= "default:leaves" and nodename ~= "default:jungleleaves"
+				and nodename ~= "default:tree" and nodename ~= "default:jungletree" then
+					surfy = y
+					break
+				end
+			end
+			if not surfy then
+				return
+			end
+			DAMAGE = false
+			crater({x=x,y=surfy,z=z},conrad)
+		end
+	end)
+end
+
+-- Abm.
+
+if ABM then
 	minetest.register_abm({
 		nodenames = {
 			"default:dirt",
@@ -37,21 +79,16 @@ if IMPACT then
 			"default:desert_sand",
 			"default:sand",
 		},    
-		interval = MSRINT,	
-		chance = MSRCHA,
+		interval = ABMINT,	
+		chance = ABMCHA,
 		action = function(pos, node, _, _)
 			local env = minetest.env
 			local x = pos.x
 			local y = pos.y
 			local z = pos.z
-			-- If in safe zone or not in impact zone then abort.
-			if (x > SAXMIN and x < SAXMAX and z > SAZMIN and z < SAZMAX)
-			or not (x > XMIN and x < XMAX and z > ZMIN and z < ZMAX) then
-				return
-			end
-			-- Find surface above, abort if underwater or no surface found.
+			-- Find close surface above, abort if underwater or no close surface found.
 			local surfy = false
-			for j = 1, 8 do
+			for j = 1, 4 do
 				local nodename = env:get_node({x=x,y=y+j,z=z}).name
 				if nodename == "default:water_source" or nodename == "default:water_flowing" then
 					return
@@ -63,23 +100,50 @@ if IMPACT then
 			if not surfy then
 				return
 			end
-			-- Check pos open to sky.
-			for j = 1, 160 do
-				local nodename = env:get_node({x=x,y=surfy+j,z=z}).name
-				if nodename ~= "air" and nodename ~= "ignore" then
-					return
-				end
-			end
-			-- Random radius.
 			local conrad = math.random(RADMIN, RADMAX)
-			local rimrad = conrad * 2.2
+			crater({x=x,y=surfy,z=z},conrad)
+		end,
+	})
+end
+
+-- Functions.
+
+function crater(pos, conrad)
+			local env = minetest.env
+			local x = pos.x
+			local y = pos.y
+			local z = pos.z
+			local rimrad = math.ceil(conrad * 2.4)
+			-- If in safe zone or not in impact zone then abort.
+			if (x > SAXMIN - rimrad and x < SAXMAX + rimrad and z > SAZMIN - rimrad and z < SAZMAX + rimrad)
+			or not (x > XMIN and x < XMAX and z > ZMIN and z < ZMAX) then
+				return
+			end
 			-- Check enough depth.
 			for j = -conrad - 1, -1 do
-				local nodename = env:get_node({x=x,y=surfy+j,z=z}).name
+				local nodename = env:get_node({x=x,y=y+j,z=z}).name
 				if nodename == "air" or nodename == "ignore" then
 					return
 				end
 			end
+			-- Check pos open to sky.
+			for j = 1, 160 do
+				local nodename = env:get_node({x=x,y=y+j,z=z}).name
+				if nodename ~= "air" and nodename ~= "ignore"
+				and nodename ~= "default:leaves" and nodename ~= "default:jungleleaves"
+				and nodename ~= "snow:ice" and nodename ~= "snow:needles" then
+					return
+				end
+			end
+			-- Excavate path.
+			for j = 1, 160 do
+				local nodename = env:get_node({x=x,y=y+j,z=z}).name
+				if nodename ~= "air" and nodename ~= "ignore" then
+					env:remove_node({x=x,y=y+j,z=z})
+				end
+			end
+			-- Add meseorite.
+			env:add_node({x=x,y=y-conrad-1,z=z},{name="default:mese"})
 			-- Excavate cone and count excavated nodes.
 			local exsto = 0
 			local exdsto = 0
@@ -87,11 +151,13 @@ if IMPACT then
 			local exdsan = 0
 			local exsan = 0
 			local extree = 0
+			local exjtree = 0
 			for j = 0, conrad * 2 do
 				for i = -j, j do
 				for k = -j, j do
 					if i ^ 2 + k ^ 2 <= j ^ 2 then
-						local nodename = env:get_node({x=x+i,y=surfy-conrad+j,z=z+k}).name
+						local exsno = false
+						local nodename = env:get_node({x=x+i,y=y-conrad+j,z=z+k}).name
 						if nodename == "default:stone" then
 							exsto = exsto + 1
 						elseif nodename == "default:desert_stone" then
@@ -104,52 +170,59 @@ if IMPACT then
 							exsan = exsan + 1
 						elseif nodename == "default:tree" then
 							extree = extree + 1
+						elseif nodename == "default:jungletree" then
+							exjtree = exjtree + 1
+						elseif nodename == "snow:snow" then
+							exsno = true
 						end
 						if nodename ~= "air" then
-							env:remove_node({x=x+i,y=surfy-conrad+j,z=z+k})
+							env:remove_node({x=x+i,y=y-conrad+j,z=z+k})
+						end
+						if exsno then
+							env:remove_node({x=x+i,y=y-conrad+j,z=z+k})
 						end
 					end
 				end
 				end
 			end
 			-- Calculate proportions of ejecta.
-			local extot = exsto + exdsto + exdirt + exdsan + exsan + extree
+			local extot = exsto + exdsto + exdirt + exdsan + exsan + extree + exjtree
 			local pexsto = exsto / extot
 			local pexdsto = exdsto / extot
 			local pexdirt = exdirt / extot
 			local pexdsan = exdsan / extot
 			local pexsan = exsan / extot
 			local pextree = extree / extot
+			local pexjtree = exjtree / extot
 			-- Print to terminal.
 			if DEBUG then
-				print ("[meseor] Radius "..conrad.." node ("..x.." "..surfy.." "..z..")")
-				print ("[meseor] exsto "..exsto.." exdsto "..exdsto.." exdirt "..exdirt.." exdsan "..exdsan.." exsan "..exsan.." extree "..extree)
+				print ("[meseor] Cone radius "..conrad.." node ("..x.." "..y.." "..z..")")
+				print ("[meseor] exsto "..exsto.." exdsto "..exdsto.." exdirt "..exdirt.." exdsan "..exdsan)
+				print ("[meseor] exsan "..exsan.." extree "..extree.." exjtree "..exjtree)
 				print ("[meseor] extot "..extot)
-				print ("[meseor] pexsto "..pexsto.." pexdsto "..pexdsto.." pexdirt "..pexdirt.." pexdsan "..pexdsan.." pexsan "..pexsan.." pextree "..pextree)
 			end
-			-- Add meseorite.
-			env:add_node({x=x,y=surfy-conrad,z=z},{name="default:mese"})
 			-- Add ejecta.
 			local addtot = 0
-			for rep = 1, 32 do
+			for rep = 1, 128 do
 				for i = -rimrad, rimrad do
 				for k = -rimrad, rimrad do
 					local rad = (i ^ 2 + k ^ 2) ^ 0.5
-					if rad <= rimrad and math.random() > math.abs(rad - conrad * 1.1) / (conrad * 1.1) and addtot < extot then
+					if rad <= rimrad and math.random() > math.abs(rad - conrad * 1.2) / (conrad * 1.2)
+					and addtot < extot and math.random(3) == 2 then
 						-- Find ground.
 						local groundy = false
 						for j = conrad - 1, -160, -1 do
-							local nodename = env:get_node({x=x+i,y=surfy+j,z=z+k}).name
+							local nodename = env:get_node({x=x+i,y=y+j,z=z+k}).name
 							if nodename == "default:leaves" or nodename == "default:jungleleaves"
 							or nodename == "default:papyrus" or nodename == "default:dry_shrub"
 							or nodename == "default:grass_1" or nodename == "default:grass_2"
 							or nodename == "default:grass_3" or nodename == "default:grass_4"
 							or nodename == "default:grass_5" or nodename == "default:apple"
-							or nodename == "default:junglegrass" then
-								env:remove_node({x=x+i,y=surfy+j,z=z+k})
-							elseif nodename ~= "air" and nodename ~= "ignore"
+							or nodename == "default:junglegrass" or nodename == "snow:needles" then
+								env:remove_node({x=x+i,y=y+j,z=z+k})
+							elseif nodename ~= "air" and nodename ~= "ignore" and nodename ~= "snow:snow"
 							and nodename ~= "default:water_source" and nodename ~= "default:water_flowing" then
-								groundy = surfy+j
+								groundy = y+j
 								break
 							end
 						end
@@ -157,7 +230,9 @@ if IMPACT then
 							local x = x + i
 							local y = groundy + 1
 							local z = z + k
-							if math.random() < pextree then
+							if math.random() < pexjtree then
+								env:add_node({x=x,y=y,z=z},{name="default:jungletree"})
+							elseif math.random() < pextree then
 								env:add_node({x=x,y=y,z=z},{name="default:tree"})
 							elseif math.random() < pexsan then
 								env:add_node({x=x,y=y,z=z},{name="default:sand"})
@@ -194,6 +269,4 @@ if IMPACT then
 					end
 				end
 			end
-		end,
-	})
 end
